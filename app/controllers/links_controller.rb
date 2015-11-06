0142d1b01a1b35329666713e5355b9f4598cf41c
Leila Jacob <leilajacob@me.com>
2015-11-06 14:12:15 -0500

    added paperclip

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -69,6 +69,6 @@ class LinksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def link_params
-      params.require(:link).permit(:link, :score, :name)
+      params.require(:link).permit(:link, :score, :name, :avatar)
     end
 end
