41b501de670214c86b27288208b82824f27a16d9
Leila Jacob <leilajacob@me.com>
2015-11-07 16:21:06 -0500

    first attempt controlling voting

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,15 +1,27 @@
 class VotesController < ApplicationController
   def upvote
   	@link = Link.find(params[:id])
-  	@link.score += 1
-  	@link.save
-  	redirect_to root_path
+
+    click = false
+
+    if click == false
+    	@link.score += 1
+    	@link.save
+    	redirect_to root_path
+      click = true
+    end
   end
 
   def downvote
   	@link = Link.find(params[:id])
-  	@link.score -= 1
-  	@link.save
-  	redirect_to root_path
+
+    click = false 
+
+    if @click == false
+    	@link.score -= 1
+    	@link.save
+    	redirect_to root_path
+      click = true
+    end
   end
 end
