07d1eaeef25d0f76f794a1d1d8c9c82e47e5976c
Leila Jacob <leilajacob@me.com>
2015-11-07 20:14:00 -0500

    added background, navavbar color, and wells, added devise to views

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,27 +1,20 @@
 class VotesController < ApplicationController
   def upvote
   	@link = Link.find(params[:id])
+    @link.score += 1
+      # if user_signed_in? 
+      #   {disable_with: "Submitting"};
+      # end
+    @link.save
+    redirect_to root_path
+    click = true
 
-    click = false
-
-    if click == false
-    	@link.score += 1
-    	@link.save
-    	redirect_to root_path
-      click = true
-    end
   end
 
   def downvote
   	@link = Link.find(params[:id])
-
-    click = false 
-
-    if @click == false
-    	@link.score -= 1
-    	@link.save
-    	redirect_to root_path
-      click = true
-    end
+    @link.score -= 1
+    @link.save
+    redirect_to root_path
   end
 end
